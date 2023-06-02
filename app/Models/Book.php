3eca91e163f1c4f7d65b446454a40d3d46d0f4ef
Victor Chinea <victord.chineap@gmail.com>
2023-06-02 03:12:56 +0200

    [V1-1][ADD] migrations and new models

---
+++ app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'book_title',
+        'authors',
+        'book_description',
+        'link',
+        'image_url',
+    ];
+}
---
+++ app/Models/Goal.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+
+class Goal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'goal_name',
+        'goal_description',
+        'days_completed',
+        'current_date',
+        'start_date',
+        'end_date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
