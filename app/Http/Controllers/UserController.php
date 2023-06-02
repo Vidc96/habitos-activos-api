f9d343e564445cfbcca73eeae58c75bd41fd26fb
Victor Chinea <victord.chineap@gmail.com>
2023-06-02 05:51:48 +0200

    [V1-1][ADD] user use cases

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all(['name', 'email']);
+
+        return response()->json($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if($user->id == 1) {
+            $user->assignRole('Admin');
+        } else {
+            $user->assignRole('User');
+        }
+
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id, ['name', 'email']);
+
+        return response()->json($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required|min:6',
+        ]);
+
+        $user->name = $request->name;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        $data = [
+            'name' => $user->name,
+            'email' => $user->email,
+        ];
+
+        return response()->json(['message' => 'User updated successfully', 'data' => $data]);
+    }
+
+    /** 
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -39,3 +39,6 @@ use Illuminate\Support\Facades\Route;
     Route::put('/books/{id}', [BookController::class, 'update']);
     Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
