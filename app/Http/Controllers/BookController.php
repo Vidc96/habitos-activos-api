2501366f851f1df5e5a9bfd6d485d7fda30442ec
Vidc96 <victord.chineap@gmail.com>
2023-06-05 00:58:30 +0200

    [V1-2][ADD] goal controller use cases

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -21,9 +21,9 @@ class BookController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($id)
     {
-        //
+    
     }
 
     /**
--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -2,83 +2,96 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Goal;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class GoalController extends Controller
 {
+    
     /**
-     * Display a listing of the resource.
+     * Display the specified goal.
      *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
-        //
+        $goals = Goal::where('user_id', $id)
+                    ->get(['id','goal_name', 'goal_description', 'days_completed', 'current_date']);
+
+        return response()->json($goals);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Create a new goal.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        
+        $data = $request->validate([
+            'goal_name' => 'required|string',
+            'goal_description' => 'required|string',
+        ]);
+
+        $data['current_date'] = Carbon::now()->format('Y-m-d');
+        $data['user_id'] = $id;
+
+        $goal = Goal::create($data);
+        return response()->json(['message' => 'User created successfully'], 201);
     }
 
+
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Check a goal.
      */
-    public function show($id)
+    public function checkGoal($id)
     {
-        //
+        $goal = Goal::findOrFail($id);
+
+        if ($goal->days_completed == 0) {
+            $goal->days_completed += 1;
+            $goal->current_date = Carbon::now()->format('Y-m-d');
+            $goal->start_date = $goal->current_date;
+            $goal->end_date = Carbon::parse($goal->start_date)->addDays(21);
+        } elseif ($goal->days_completed > 0 && Carbon::parse($goal->current_date)->addDay(1)->isSameDay(Carbon::now())) {
+            $goal->days_completed += 1;
+            $goal->current_date = Carbon::now()->format('Y-m-d');
+            $goal->start_date = $goal->current_date;
+            $goal->end_date = Carbon::parse($goal->start_date)->addDays(21);
+        } elseif ($goal->days_completed > 0 && $goal->current_date != Carbon::now()->subDay()->format('Y-m-d')) {
+            $goal->days_completed = 1;
+            $goal->current_date = Carbon::now()->format('Y-m-d');
+            $goal->start_date = $goal->current_date;
+            $goal->end_date = Carbon::parse($goal->start_date)->addDays(21);
+        } elseif ($goal->days_completed > 0 && $goal->current_date == Carbon::now()->subDay()->format('Y-m-d')) {
+            $goal->days_completed += 1;
+            $goal->current_date = Carbon::now()->format('Y-m-d');
+        }
+
+        if ($goal->days_completed == 20) {
+            $this->destroy($id);
+        } else {
+            $goal->save();
+        }
+
+        return response()->json($goal);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
+     * Remove the specified goal from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        $goal = Goal::findOrFail($id);
+        $goal->delete();
+        return response()->json(null, 204);
+        
     }
 }
