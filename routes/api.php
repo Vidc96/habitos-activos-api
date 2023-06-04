1457be1215c1055e88072251349a3b2d6366ddd3
Vidc96 <victord.chineap@gmail.com>
2023-06-05 01:14:45 +0200

    [V1-2][FIX] routes book

--- routes/api.php
+++ routes/api.php
@@ -36,9 +36,7 @@ use Illuminate\Support\Facades\Route;
 
     // Book routes
     Route::get('/books', [BookController::class, 'index']);
-    Route::get('/books/{id}', [BookController::class, 'show']);
     Route::post('/books', [BookController::class, 'store']);
-    Route::put('/books/{id}', [BookController::class, 'update']);
     Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
