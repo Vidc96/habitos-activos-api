91846a03fa0e8782806ea050f12002f120c80671
Vidc96 <victord.chineap@gmail.com>
2023-06-06 00:04:18 +0200

    [V1-2][ADD] test

--- tests/Feature/GoalControllerTest.php
+++ tests/Feature/GoalControllerTest.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\User;
+use App\Models\Goal;
 
 class GoalCOntrollerTest extends TestCase
 {
